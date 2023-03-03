90e830841c8084a74f6243662c96ef475eadd0b4
akash <akash.priyadarshi@knoldus.com>
2023-03-03 11:45:09 +0530

    Added mapping code

---
+++ mapping.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import requests
+import numpy as np
+
+url = 'https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json'
+d = requests.get(url).json()
+token_df = pd.DataFrame.from_dict(d)
+token_df['expiry'] = pd.to_datetime(token_df['expiry'])
+token_df = token_df.astype({'strike': float})
+
+#print(token_df)
+
+def getTokenInfo (df, exch_seg, instrumenttype,symbol,strike_price,pe_ce):
+    #df = credentials.TOKEN_MAP
+    strike_price = strike_price*100
+    if exch_seg == 'NSE':
+        eq_df = df[(df['exch_seg'] == 'NSE') & (df['symbol'].str.contains('EQ')) ]
+        return eq_df[eq_df['name'] == symbol]
+    elif exch_seg == 'NFO' and ((instrumenttype == 'FUTSTK') or (instrumenttype == 'FUTIDX')):
+        return df[(df['exch_seg'] == 'NFO') & (df['instrumenttype'] == instrumenttype) & (df['name'] == symbol)].sort_values(by=['expiry'])
+    elif exch_seg == 'NFO' and (instrumenttype == 'OPTSTK' or instrumenttype == 'OPTIDX'):
+        return df[(df['exch_seg'] == 'NFO') & (df['instrumenttype'] == instrumenttype) & (df['name'] == symbol) & (df['strike'] == strike_price) & (df['symbol'].str.endswith(pe_ce))].sort_values(by=['expiry'])
+
+
+print(getTokenInfo(token_df,'NSE','OPTIDX','SBIN',14900,'PE'))
+print(getTokenInfo(token_df,'NFO','OPTIDX','BANKNIFTY',40200,'PE'))
+
